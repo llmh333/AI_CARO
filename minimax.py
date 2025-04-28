ee499b91e6f971c5d0038cd6db08fa111fea8e38
LeMinhHieu <lminhhieu2507@gmail.com>
2025-04-29 01:13:57 +0700

    feat: add alpha-beta pruning algorithm

--- minimax.py
+++ minimax.py
@@ -27,7 +27,7 @@ class AI:
     def get_all_move_possible(self):
         dx = [-1, 1, 0, 0, -1, 1, -1, -1]
         dy = [0, 0, -1, 1, 1, 1, -1, 1]
-        moves = []
+        moves = set()
         for row in range(BOARD_ROWS):
             for col in range(BOARD_COLUMNS):
                 if (self.board[row][col] != None):
@@ -36,8 +36,11 @@ class AI:
                         col_new = col + dy[i]
                         if row_new >= 0 and row_new < BOARD_ROWS and col_new >= 0 and col_new < BOARD_COLUMNS:
                             if (self.board[row_new][col_new] == None) and (not(row_new,col_new) in moves):
-                                moves.append((row_new,col_new))
-        return moves
+                                moves.add((row_new,col_new))
+        moves = list(moves)
+        center = (BOARD_ROWS // 2, BOARD_COLUMNS // 2)
+        moves.sort(key=lambda m: abs(m[0] - center[0]) + abs(m[1] - center[1]))
+        return list(moves)
 
     def check_status_game(self, move, typeChess):
         direct = [
@@ -66,7 +69,8 @@ class AI:
         if self.is_board_full():
             return [TIE_GAME, typeChess]          
 
-    def minimax(self, depth, is_maximizing, move: tuple([int, int]), type_chess):
+    
+    def minimax(self, depth, is_maximizing, move: tuple([int, int]), type_chess, alpha : float = -float('inf'), beta : float = float('inf')):
         check_status = self.check_status_game(move, type_chess)
         if check_status != None:
             if check_status[0] == WIN_GAME and check_status[1] == X_TURN:
@@ -75,43 +79,50 @@ class AI:
                 return 10-depth
             elif check_status[0] == TIE_GAME:
                 return 0
-        # if depth == 4:
-        #     return 0
+        if depth == 5:
+            return 0
         moves = self.get_all_move_possible()
+        if not moves:
+            return self.evaluate_board(self.board, O_TURN)
         if is_maximizing:
-            best_score = -1000
+            best_score = float('-inf')
             for move in moves:
                 self.make_move(move, O_TURN)
-                score = self.minimax(depth+1, False, move, O_TURN)
+                score = self.minimax(depth+1, False, move, O_TURN, alpha, beta)
                 self.remake_move(move)
                 best_score = max(score,best_score)
+                alpha = max(alpha, score)
+                if beta <= alpha:
+                    break
             return best_score
         else:
-            best_score = 1000
+            best_score = float('inf')
             for move in moves:
                 self.make_move(move, X_TURN)
-                score = self.minimax(depth+1, True, move, X_TURN)
+                score = self.minimax(depth+1, True, move, X_TURN, alpha, beta)
                 self.remake_move(move)
                 best_score = min(score,best_score)
+                beta = min(beta, score)
+                if beta <= alpha:
+                    break
             return best_score
 
 
     def best_move(self):
-        best_score = -1000
-        move = (-1, -1)
+        best_score = -float('inf')
+        move = None
         moves = self.get_all_move_possible()
         if not moves:
             return None
+        
         for move_possible in moves:
             self.make_move(move_possible,O_TURN)
-            score = self.minimax(0, False, move_possible, O_TURN)
+            score = self.minimax(0, False, move_possible, O_TURN, -float('inf'), float('inf'))
             self.remake_move(move_possible)
             if score > best_score:
                 best_score = score
                 move = move_possible
-        if (move != (-1,-1)):
-            return move
-        return None
+        return move
 
 # board = np.full((BOARD_COLUMNS,BOARD_ROWS), None, dtype=object)
 # board[0][0] = O_TURN
